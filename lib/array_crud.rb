f95d2c93297b157b7725d8e28a9fdd99ab9f32f7
calm-fork-7535 <komald.4@gmail.com>
2020-01-17 17:55:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array["bye"] << "Hello"
+  array["I", "am", "really", "learning"] << "Hello"
 end
 
 def add_element_to_start_of_array(array, element)
