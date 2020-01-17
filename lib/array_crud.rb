c8668b51d19c91b9cf8caf738d294b4810b994ad
calm-fork-7535 <komald.4@gmail.com>
2020-01-17 17:53:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << "Hello"
+  add_element_to_end_of_array << "Hello"
 end
 
 def add_element_to_start_of_array(array, element)
